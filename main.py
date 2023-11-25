a99c721a0349264534fa4931bd3526579037dd47
diegodslima <diegodslima92@gmail.com>
2023-11-25 12:05:55 -0300

    fastapi inha endpoint

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 from src.classes.Dataset import Dataset
-        
-smiles_path = "src/dataset/example/best_ligands.smi"
-dataset = Dataset(smiles_path)
+from fastapi import FastAPI, File, UploadFile
+import json
 
-dataset.create_dataframe()
-dataset.calculate_mordred()
-dataset.mlinha_predict()
+app = FastAPI()
 
-print(dataset.inha_prediction)
+@app.get("/")
+def read_root():
+    return {"message": "App running."}
+
+@app.post("/inhA_pred")
+async def inha_prediction(file: UploadFile = File(...)):
+
+    with open(f"temp/{file.filename}", "wb") as f:
+        f.write(file.file.read())
+
+    file_path = f"temp/{file.filename}"
+
+    dataset = Dataset(file_path)
+    dataset.create_dataframe()
+    dataset.calculate_mordred()
+    dataset.mlinha_predict()
+    
+    parsed_data = json.loads(dataset.inha_prediction.write_json(row_oriented=True))
+    
+    return parsed_data
