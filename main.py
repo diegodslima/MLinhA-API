d4c65d1f6c87534a107896eb4825144631dfa058
diegodslima <diegodslima92@gmail.com>
2023-11-25 12:41:13 -0300

    delete temp files and add uuid filename

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from src.classes.Dataset import Dataset
 from fastapi import FastAPI, File, UploadFile
 import json
+import os
+import uuid
 
 app = FastAPI()
 
@@ -10,17 +12,26 @@ def read_root():
 
 @app.post("/inhA_pred")
 async def inha_prediction(file: UploadFile = File(...)):
+    try:
+        
+        unique_id = str(uuid.uuid4())
+        file_extension = os.path.splitext(file.filename)[1]
+        new_filename = f"{unique_id}{file_extension}"
+        print(new_filename)
 
-    with open(f"temp/{file.filename}", "wb") as f:
-        f.write(file.file.read())
+        with open(f"temp/{new_filename}", "wb") as f:
+            f.write(file.file.read())
 
-    file_path = f"temp/{file.filename}"
+        file_path = f"temp/{new_filename}"
 
-    dataset = Dataset(file_path)
-    dataset.create_dataframe()
-    dataset.calculate_mordred()
-    dataset.mlinha_predict()
+        dataset = Dataset(file_path)
+        dataset.create_dataframe()
+        dataset.calculate_mordred()
+        dataset.mlinha_predict()
+        
+        parsed_data = json.loads(dataset.inha_prediction.write_json(row_oriented=True))
+        
+        return parsed_data
     
-    parsed_data = json.loads(dataset.inha_prediction.write_json(row_oriented=True))
-    
-    return parsed_data
+    finally:
+        os.remove(file_path)
