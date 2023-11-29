3735950210cf670d06d65d2e70bda9624ec5552f
diegodslima <diegodslima92@gmail.com>
2023-11-29 11:02:35 -0300

    added MAX_LINES = 1000

--- src/functions/rewriteSmilesFile.py
+++ src/functions/rewriteSmilesFile.py
@@ -10,10 +10,21 @@ def correctFormat(string1, string2):
     return f"{string1} {string2}"
 
 def rewriteSmilesFile(input_file_path, output_file_path):
+    MAX_LINES = 1000
     with open(input_file_path, 'r') as input_file, open(output_file_path, 'w') as output_file:
-        for line in input_file:
-            strings = extractStrings(line)
-            
-            if len(strings) == 2:
-                corrected_line = correctFormat(strings[0], strings[1])
-                output_file.write(corrected_line + '\n')
+        input_lines = input_file.readlines()
+        if len(input_lines) > MAX_LINES:
+            print("Maximum lines exceeded.")
+            for line in input_lines[:MAX_LINES]:
+                strings = extractStrings(line)
+                
+                if len(strings) == 2:
+                    corrected_line = correctFormat(strings[0], strings[1])
+                    output_file.write(corrected_line + '\n')            
+        else:
+            for line in input_lines:
+                strings = extractStrings(line)
+                
+                if len(strings) == 2:
+                    corrected_line = correctFormat(strings[0], strings[1])
+                    output_file.write(corrected_line + '\n')
